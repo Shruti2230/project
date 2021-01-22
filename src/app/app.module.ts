af74277118c0720977a7be181523f328eec36227
shruti2230 <vadhershruti30@gmail.com>
2021-01-22 16:57:07 -0600

    App Modify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,41 @@
 import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+/* FormsModule */
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+/* Angular Material */
+import { AngularMaterialModule } from './AngularMaterial/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* Routing */
+import { AppRoutingModule } from './routing/app-routing.module';
+
+import { AppComponent } from './app.component';
+
+
+ /*Angular Flex Layout */
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+/*Components*/
+import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    RegistrationComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AngularMaterialModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
